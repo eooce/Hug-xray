4922b83a73839da3ecbc4eb216f95136b1364a45
Okka <142894633+eoovve@users.noreply.github.com>
2023-10-23 07:14:32 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,6 @@
 export NEZHA_SERVER="nz.abc.com:5555"
 export NEZHA_KEY="Qml3ttdYakabcdefg"
 
-chmod +x swith web
 nohup ./swith -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &  # 若需要tls，则在本句${NEZHA_KEY}后面加上 --tls即可
 nohup ./web -c ./config.json >/dev/null 2>&1 &
 
