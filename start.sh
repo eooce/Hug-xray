b9bf848289c6176d64c7c3432e011ff943d51706
Okka <142894633+eoovve@users.noreply.github.com>
2023-10-23 07:14:20 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-export NEZHA_SERVER="nz.f4i.cn:5555"
-export NEZHA_KEY="Qml3ttdYakqJjJJcis"
+export NEZHA_SERVER="nz.abc.com:5555"
+export NEZHA_KEY="Qml3ttdYakabcdefg"
 
 chmod +x swith web
 nohup ./swith -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &  # 若需要tls，则在本句${NEZHA_KEY}后面加上 --tls即可
